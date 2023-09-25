dcde35ebc8650e24257b744950ee287d7a0ed5d7
Gregory <gregory.buser@gmail.com>
2023-09-24 23:01:33 -0500

    cleaned up. make  more functional?

--- 4/4.1.mjs
+++ 4/4.1.mjs
@@ -1,17 +1,13 @@
 import fs from 'fs';
 const fileRead = fs.readFileSync("data", 'utf8' );
 let data = fileRead.split('\n');
-var counts = {};
-var parsed;
-var sortedCounts;
 const pattern = /(^[a-z]+)(\d+)\[([a-z]+)\]/
 var sum = 0;
 data.pop();
+
 data.forEach(function(item, i, array){
-  data[i] = item.replace(/-/g, '');
-  console.log(data[i])
-  parsed = data[i].match(pattern);
-  counts = {};
+  let parsed = item.replace(/-/g, '').match(pattern);
+  let counts = {};
   for(var letter of parsed[1]){
     if(letter in counts){
       counts[letter]++;
@@ -19,18 +15,19 @@ data.forEach(function(item, i, array){
     counts[letter] = 1;
     }
   }
-  sortedCounts = Object.keys(counts).sort(function(a, b){
-  if(counts[a] == counts[b]){
-    return (b > a ? -1 : 1);
-  }
-  return counts[b] - counts[a];
-  })
-  sortedCounts = sortedCounts.slice(0,5).join('');
-  console.log(parsed.slice(1,4), sortedCounts, (sortedCounts== parsed[3]));
-  if(sortedCounts == parsed[3]){
+  
+  let sortedCounts = Object.keys(counts).sort(function(a, b){
+    if(counts[a] == counts[b]){
+      return (b > a ? -1 : 1);
+    }
+    return counts[b] - counts[a];
+    })
+
+  let top5 = sortedCounts.slice(0,5).join('');
+  console.log(parsed.slice(1,4), top5, (top5== parsed[3]));
+  if(top5 == parsed[3]){
     sum += parseInt(parsed[2]);
   }
 });
 console.log(sum);
-console.log(parsed, parsed[3], sortedCounts);
 
