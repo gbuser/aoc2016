3f8d05eb29c587e69285c97bccc4f0b3356ee868
Gregory <gregory.buser@gmail.com>
2023-09-25 00:16:15 -0500

    made part one as functional as i can

---
+++ 4/4.1.f.mjs
@@ -0,0 +1,33 @@
+import fs from 'fs';
+const fileRead = fs.readFileSync("data", 'utf8' );
+let data = fileRead.split('\n');
+data.pop();
+//regex creates three option groups- initial string, sector and checksum 
+const pattern = /(^[-a-z]+)(\d+)\[([a-z]+)\]/ 
+
+let parsed = data.map(item => {
+  return item.match(pattern); //returns match object array (0: original, 1-3: capture groups)
+})
+let filtered = parsed.filter(item =>{
+  return topFive(item[1]) == item[3]; //returns lines where sorted top five = checksum
+})
+let answer = filtered.reduce((accumulator, item)=>
+  accumulator + parseInt(item[2]), 0); //reduces filtered lines to sum of sectors
+console.log(answer);
+//could combine the map/filter/reduce by .'s, but would be ugly. 
+
+function topFive(text){  //takes the text portion and returns top five letters, sorted
+  let counts = {};
+  for(let letter of text){
+    if(letter == '-') continue; //remove -'s lest they be counted
+    if(letter in counts) counts[letter]++;
+    else counts[letter] = 1;
+  }
+  let sortedFive = Object.keys(counts).sort((a,b) =>{
+    if(counts[a] == counts[b]){ //secondary sort if same occurences is alphabetical
+      return(b > a ? -1 : 1);  //return (b-a) fails as char math not allowed
+    }
+    return counts[b] - counts[a]; //primary sort by number of occurences
+  }).slice(0,5).join(''); //take the top 5 and join into one string
+  return sortedFive;
+}
