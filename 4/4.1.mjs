0447dd017d96531b055a0084b00ed02563a12f1a
Gregory <gregory.buser@gmail.com>
2023-09-24 22:44:41 -0500

    works, needs cleaned up

--- 4/4.1.mjs
+++ 4/4.1.mjs
@@ -1,31 +1,36 @@
 import fs from 'fs';
 const fileRead = fs.readFileSync("data", 'utf8' );
 let data = fileRead.split('\n');
-console.log(data[0]);
+var counts = {};
+var parsed;
+var sortedCounts;
+const pattern = /(^[a-z]+)(\d+)\[([a-z]+)\]/
+var sum = 0;
+data.pop();
 data.forEach(function(item, i, array){
   data[i] = item.replace(/-/g, '');
-})
-var counts;
-
-console.log(data[0])
-const pattern = /(^[a-z]+)(\d+)\[([a-z]+)\]/
-var parsed = data[0].match(pattern);
-console.log(parsed);
-counts = {};
-for(var letter of parsed[1]){
-  if(letter in counts){
-    counts[letter]++;
-  } else {
+  console.log(data[i])
+  parsed = data[i].match(pattern);
+  counts = {};
+  for(var letter of parsed[1]){
+    if(letter in counts){
+      counts[letter]++;
+    } else {
     counts[letter] = 1;
+    }
   }
-  }
-console.log(counts);
-var sortedCounts = Object.keys(counts).sort(function(a, b){
+  sortedCounts = Object.keys(counts).sort(function(a, b){
   if(counts[a] == counts[b]){
     return (b > a ? -1 : 1);
   }
   return counts[b] - counts[a];
-})
-console.log(sortedCounts);
-sortedCounts = sortedCounts.slice(0,5).join('');
-console.log(sortedCounts);
+  })
+  sortedCounts = sortedCounts.slice(0,5).join('');
+  console.log(parsed.slice(1,4), sortedCounts, (sortedCounts== parsed[3]));
+  if(sortedCounts == parsed[3]){
+    sum += parseInt(parsed[2]);
+  }
+});
+console.log(sum);
+console.log(parsed, parsed[3], sortedCounts);
+
