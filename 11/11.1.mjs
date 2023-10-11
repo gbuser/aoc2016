bda31666deb026b4fb17d73e2c3cb23d62ebe58d
Gregory <gregory.buser@gmail.com>
2023-10-11 16:29:33 -0500

    before messing w listMoves

--- 11/11.1.mjs
+++ 11/11.1.mjs
@@ -1,38 +1,44 @@
 import fs from 'fs';
 import percom from 'percom'
 const data = fs.readFileSync("data", 'utf8' ).trim().split("\n");
-
 const CG = 512, PG = 256, RG = 128, SG = 64, TG = 32;//make obj {CG:512, PG:256...}
 const CM = 16, PM = 8, RM = 4, SM = 2, TM = 1;
-const generators = [CG, PG, RG, SG, TG];
-const modules = [CM, PM, RM, SM, TM];
-let elevator = 1;
-let floors = [[], [SG,SM, PG, PM], [TG, RG, RM, CG, CM], [TM], []]
+let floors = [, SG+SM+PG+PM, TG+RG+RM+CG+CM, TM, 0]
 data.forEach(line => console.log(line))
 floors.forEach( floor => console.log(`${floors.indexOf(floor)}: ${floor}`))
-//list.forEach( item => move(item, 1, 2))
-console.log(testElevator(2));
-let goodElv = new Set();
-let goodFloor = new Set();
-for(let i = 0; i < 1024 ; i++){
-  if(testElevator(i)) goodElv.add(i);
-  if(testFloor(i)) goodFloor.add(i);
-}
-console.log (goodElv.size, goodFloor.size)
-let moveList = listMoves(floors[3]);
+const [goodElv, goodFloor] = [...makeGoods()];
+let elevator = 1;
+let moveList = listMoves(floors[elevator]);
 
-function listMoves(arr){
+
+console.log(moveList.length);
+
+function move(item, direction){
+  if(goodFloor.has(floors[from] - item)) console.log("original floor good");
+  else console.log("original floor bad")
+  if(goodFloor.has(floors[to] +  item)) console.log("new floor good\n");
+  else console.log("new floor bad\n");
+
+}
+function listMoves(value){
   let moveList = [];//array of possible moves
-  arr.forEach( item => moveList.push([item]));//add singles
+  let arr = [];
+  for(let i = 0; i < 10 ; i++){
+    if(value >> i & 1) arr.push(2**i);//creates array of elements
+  }
+  arr.forEach( item => moveList.push(item));//add singles
+  //arr.forEach( item => moveList.push([item]));//add singles
   if(arr.length < 2) return moveList;
   let perms = percom.com(arr,2);//generate array of all perms of arr choose 2
   moveList.push(...perms);//movelist now has all possible, some illegal
   moveList.forEach( (innerArr, index) => {
     moveList[index] = innerArr.reduce((acc, cur) => acc + cur, 0)
   })//replaces each moveList array with its sum
+  console.log(`move list now has ${moveList.length}`);
+  moveList = moveList.filter( move => goodElv.has(move));//remove illegal elevator combos
+  console.log(`filtered move list now has ${moveList.length}`);
   return moveList;
 }
-
 function testElevator(value){ //takes a bitfield as integer
   if(value == 0) return false;//empty elevator dont run
   let arr = [];
@@ -47,9 +53,26 @@ function testElevator(value){ //takes a bitfield as integer
 }
 function testFloor(value){
   for(let i = 0 ; i < 5 ; i++){
-    if((value >> i) && (!(value >> (i + 5)))){// check each module for its gen
+    if(((value >> i) & 1) && (!(value >> (i + 5) & 1))){// check each module for its gen
       if(value > 15) return false;//if no matching gen and any gens, fry
     }
   }
   return true;
+}
+function makeGoods(){
+  let goodElv = new Set();
+  let goodFloor = new Set();
+  for(let i = 0; i < 1024 ; i++){
+    if(testElevator(i)) goodElv.add(i);
+    if(testFloor(i)) goodFloor.add(i);
+  }
+  return [goodElv, goodFloor];
+}
+function digest(floors){
+  let result = '';
+  floors.forEach(floor => {
+    result += floor.toString();
+    result += '.';
+  })
+  return result;
 }
